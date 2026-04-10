1054aa5bf474d4af22512cbcedcd011aca552462
lmcr136a <lmcr136a@g.ucla.edu>
2026-04-10 10:53:15 -0700

    .

--- launch_files/launch_slam.sh
+++ launch_files/launch_slam.sh
@@ -12,12 +12,19 @@ source "$WORKSPACE_DIR/livox_driver2_ws/install/setup.bash"
 # source "$WORKSPACE_DIR/fast_lio_ws/install/setup.bash"
 # ros2 launch fast_lio mapping.launch.py config_path:=/home/nahyeon/box/vln-large-scale-farm/fast_lio_ws/src/FAST_LIO/config config_file:=1scout_livox.yaml
 
+export LD_LIBRARY_PATH=/home/nahyeon/box/vln-large-scale-farm/glim_ws
 source "$WORKSPACE_DIR/glim_ws/install/setup.bash"
 
 # Start glim_converters in background
 ros2 run glim_converters glim_converters &
 CONVERTER_PID=$!
 sleep 1
-ros2 run glim_ros glim_rosnode --ros-args -p config_path:=/home/nahyeon/box/vln-large-scale-farm/glim_ws/config
+# ros2 run glim_ros glim_rosnode --ros-args -p config_path:=/home/nahyeon/box/vln-large-scale-farm/glim_ws/config
+ros2 run glim_ros glim_rosnode \
+  --ros-args \
+  -p config_path:=/home/nahyeon/box/vln-large-scale-farm/glim_ws/config \
+  -p localizer_ext.map_path:=/home/nahyeon/box/glim_map
+
+
 
 trap "kill $CONVERTER_PID 2>/dev/null" EXIT
