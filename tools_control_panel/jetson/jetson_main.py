00f9ff34c087146ff560a3fa484e018bf6aa0aad
lmcr136a <lmcr136a@gmail.com>
2026-05-14 14:18:15 -0700

    .

--- tools_control_panel/jetson/jetson_main.py
+++ tools_control_panel/jetson/jetson_main.py
@@ -261,7 +261,7 @@ def main():
         while not shutdown.is_set():
             if shutdown.wait(timeout=interval):
                 break
-            if not internet.connected or internet.quality == "low":
+            if not internet.connected or internet._quality == "low":
                 continue
             try:
                 mtime = os.path.getmtime(png_path)
